019ce89b0c2c1fa25dfff50df5186a60e229b0b4
Bruno <69259703+bpcavalcante@users.noreply.github.com>
2021-05-06 19:15:18 -0300

    Criando Layers

---
+++ backend/src/main/java/com/vendas/dashvendas/service/SellerService.java
@@ -0,0 +1,25 @@
+package com.vendas.dashvendas.service;
+
+import java.util.List;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.vendas.dashvendas.dto.SellerDTO;
+import com.vendas.dashvendas.entites.Seller;
+import com.vendas.dashvendas.repositories.SellerRepository;
+
+@Service
+public class SellerService {
+
+	@Autowired
+	private SellerRepository repository;
+	
+	public List<SellerDTO> findAll(){
+		List<Seller> result = repository.findAll();
+		return result.stream().map(x -> new SellerDTO(x)).collect(Collectors.toList());
+	}
+	
+}
